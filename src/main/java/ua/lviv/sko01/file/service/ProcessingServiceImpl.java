23ae0691c95a94c4ef7485369ed752a702f1b661
sko01 <skovalenko1@gmail.com>
2017-03-10 13:46:38 +0200

    Property file was added

--- src/main/java/ua/lviv/sko01/file/service/ProcessingServiceImpl.java
+++ src/main/java/ua/lviv/sko01/file/service/ProcessingServiceImpl.java
@@ -1,15 +1,74 @@
 package ua.lviv.sko01.file.service;
 
+import java.io.InputStream;
+import java.util.Date;
+import java.util.Properties;
+
+import org.apache.log4j.Logger;
+
 import ua.lviv.sko01.dto.DocumentDTO;
 import ua.lviv.sko01.file.util.DocumentProcessingUtils;
 import ua.lviv.sko01.hibernate.model.Document;
 
 public class ProcessingServiceImpl implements ProcessingService{
+	private static final Logger LOG = Logger.getLogger(ProcessingServiceImpl.class);
+	private static final String xmlExtention = ".xml";
+	private static final String pdfExtention = ".pdf";
+	private String formName;
+	private String filePath;
 	
+	public ProcessingServiceImpl() {
+		loadProjectProperties();
+	}
+
 	public DocumentDTO createPDFRepresentation(Document document, DocumentDTO documentDTO){
 		documentDTO.setId(document.getId());
-		String fileName = DocumentProcessingUtils.saveXmlDataToFolder(document);
-		DocumentProcessingUtils.getPdfData(fileName, documentDTO);
-		return documentDTO;
+		String fileName = saveXmlDataToFolder(document);
+		
+		if(fileName != null && !fileName.isEmpty()){
+			String fullDataFilePath = filePath + "\\" + fileName + xmlExtention;
+			String fullOutputFilePath = filePath + "\\" + fileName + pdfExtention;
+			String fullFormPath = filePath + "\\" + formName;
+			LOG.info("Form path:" + fullFormPath);
+			LOG.info("Data path:" + fullDataFilePath);
+			LOG.info("Output path:" + fullOutputFilePath);
+			
+			if(DocumentProcessingUtils.generatePdfFromXml(fullFormPath, fullDataFilePath, fullOutputFilePath)){
+				DocumentProcessingUtils.loadFileAsBytesArray(fullOutputFilePath, documentDTO);
+				DocumentProcessingUtils.deleteTemporaryFiles(fullOutputFilePath);
+			}
+			else {
+				documentDTO.getErrors().add("Failed to create PDF representation");
+			}
+			DocumentProcessingUtils.deleteTemporaryFiles(fullDataFilePath);
+		}
+		return documentDTO;		
+	}
+	
+	private String saveXmlDataToFolder(Document document) {
+		String fileName = null;
+		Date date = new Date();
+		if(document != null){
+			fileName = document.getId() + "_" + date.getTime();
+			byte[] content = document.getDocData();
+			DocumentProcessingUtils.createFolder(filePath);
+			DocumentProcessingUtils.writeByteArraysToFile(filePath + "\\" + fileName + xmlExtention, content);
+		}
+		return fileName;
+	}
+	
+	private void loadProjectProperties(){
+		Properties prop = new Properties();
+		try{
+			InputStream inputStream = getClass().getClassLoader().getResourceAsStream("project.properties");
+			prop.load(inputStream);
+			filePath = prop.getProperty("processing.folder");
+			formName = prop.getProperty("processing.form.name");
+		} catch (Exception ex) {
+			LOG.warn("Failed to load property file", ex);
+			filePath = "C:\\processing_folder";
+			formName = "Sample_Invoice.xatw";
+		}
+		
 	}
 }
--- src/main/java/ua/lviv/sko01/file/util/DocumentProcessingUtils.java
+++ src/main/java/ua/lviv/sko01/file/util/DocumentProcessingUtils.java
@@ -6,42 +6,16 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.Date;
 
 import org.apache.log4j.Logger;
 
 import ua.lviv.sko01.dto.DocumentDTO;
-import ua.lviv.sko01.hibernate.model.Document;
 
 public class DocumentProcessingUtils {
 	private static final Logger LOG = Logger.getLogger(DocumentProcessingUtils.class); 
-	private static final String filePath = "C:\\processing_folder";
-	private static final String xmlExtention = ".xml";
-	private static final String pdfExtention = ".pdf";
-
-	public static String saveXmlDataToFolder(Document document) {
-		Date date = new Date();
-		String fileName = document.getId() + "_" + date.getTime();
-		byte[] content = document.getDocData();
-		writeByteArraysToFile(fileName + xmlExtention, content);
-		return fileName;
-	}
 	
-	public static void getPdfData(String fileName, DocumentDTO documentDTO) {
-		if(generatePdfFromXml(fileName)) {
-			deleteTemporaryFiles(fileName + xmlExtention);
-			loadFileAsBytesArray(fileName + pdfExtention, documentDTO);
-		} else {
-			deleteTemporaryFiles(fileName + xmlExtention);
-			documentDTO.getErrors().add("Failed to create PDF representation");
-		}
-	}
-
-	public static boolean generatePdfFromXml(String fileName) {
+	public static boolean generatePdfFromXml(String fullFormPath, String fullDataFilePath, String fullOutputFilePath) {
 		Process process;
-		String fullDataFilePath = filePath + "\\" + fileName + xmlExtention;
-		String fullOutputFilePath = filePath + "\\" + fileName + pdfExtention;
-		String fullFormPath = filePath + "\\Sample_Invoice.xatw";
 		//String command = String.format("%1s\\process.bat %2s %3s", filePath, filePath + "\\" + fileName + xmlExtention, filePath + "\\" + fileName + pdfExtention);
 		String command = String.format("merge.exe -form %1s -data %2s -output %3s", fullFormPath, fullDataFilePath, fullOutputFilePath);
 		try{
@@ -59,7 +33,7 @@ public class DocumentProcessingUtils {
 		}
 	}
 	
-	private static void writeByteArraysToFile(String fileName, byte[] content){
+	public static void createFolder(String filePath){
 		File dir = new File(filePath);
 		if(!dir.exists()){
 			if(dir.mkdir()){
@@ -68,7 +42,10 @@ public class DocumentProcessingUtils {
 				LOG.warn("Failed to create " + filePath + " folder");
 			}
 		}
-		File file = new File(filePath + "\\" + fileName);
+	}
+	
+	public static void writeByteArraysToFile(String fileName, byte[] content){	
+		File file = new File(fileName);
 		try {
 			file.createNewFile();
 			BufferedOutputStream writer = new BufferedOutputStream(new FileOutputStream(file));
@@ -81,8 +58,8 @@ public class DocumentProcessingUtils {
 		}
 	}
 	
-	private static void loadFileAsBytesArray(String fileName, DocumentDTO documentDTO){
-		File file = new File(filePath + "\\" + fileName);
+	public static void loadFileAsBytesArray(String fileName, DocumentDTO documentDTO){
+		File file = new File(fileName);
         int length = (int) file.length();
         byte[] bytes = new byte[length];
         try {
@@ -90,15 +67,14 @@ public class DocumentProcessingUtils {
 			reader.read(bytes, 0, length);
 			reader.close();
 			documentDTO.setData(bytes);
-			deleteTemporaryFiles(fileName);
 		} catch (IOException e) {
 			e.printStackTrace();
 			documentDTO.getErrors().add(e.getMessage());
 		}
 	}
 	
-	private static void deleteTemporaryFiles(String fileName){
-		File file = new File(filePath + "\\" + fileName);
+	public static void deleteTemporaryFiles(String fileName){
+		File file = new File(fileName);
 		if(file.delete()){
 			LOG.info("Deleted " + fileName + " file");
 		} else {
